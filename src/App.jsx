fc10f1c4f62b14d399053da56d9663d23c2e9eb5
Tran Xuan Binh <binidafs2k1@gmail.com>
2026-07-15 22:14:06 +0700

    chore: add assets and install dependencies

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="text-3xl text-orange-600 relative h-dvh">
+      <h1 className="leading-15 bg-amber-100 -rotate-3">Hello GSAP</h1>
+    </div>
+  );
+};
+
+export default App;
