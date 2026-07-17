9dc386c10cc828135b07ee7eab4b2091b74d3e7b
Tran Xuan Binh <binidafs2k1@gmail.com>
2026-07-18 02:38:57 +0700

    feat: implement footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,93 @@
+import { useResponsive } from "../libs/useResponsive";
+
+const FooterSection = () => {
+  const { isMobile } = useResponsive();
+
+  return (
+    <section className="footer-section z-10">
+      <img
+        src="/images/footer-dip.png"
+        alt=""
+        className="w-full object-cover  md:-translate-y-10 z-30"
+      />
+
+      <div className="2xl:h-[110dvh] relative md:pt-[12vh] pt-[8vh]">
+        <div className="overflow-hidden z-10">
+          <h1 className="general-title text-center md:mt-4 text-milk py-6 opacity-0 md:opacity-100">
+            #CHUGRESPONSIBLY
+          </h1>
+        </div>
+
+        {isMobile ? (
+          <img
+            src="/images/footer-drink.png"
+            className="absolute top-0 object-contain"
+          />
+        ) : (
+          <video
+            src="/videos/splash.mp4"
+            autoPlay
+            playsInline
+            muted
+            className="absolute top-20 object-contain mix-blend-lighten"
+          />
+        )}
+
+        <div className="flex-center gap-5 relative z-10 md:mt-2 mt-6 ml-35">
+          <div className="social-btn">
+            <img src="./images/yt.svg" alt="" />
+          </div>
+          <div className="social-btn">
+            <img src="./images/insta.svg" alt="" />
+          </div>
+          <div className="social-btn">
+            <img src="./images/tiktok.svg" alt="" />
+          </div>
+        </div>
+
+        <div className="mt-20 md:px-10 px-5 flex gap-10 md:flex-row flex-col justify-between text-milk font-paragraph md:text-lg font-medium">
+          <div className="flex items-center md:gap-16 gap-5">
+            <div>
+              <p>SPYLT Flavors</p>
+            </div>
+            <div>
+              <p>Chug Club</p>
+              <p>Student Marketing</p>
+              <p>Dairy Dealers</p>
+            </div>
+            <div>
+              <p>Company</p>
+              <p>Contacts</p>
+              <p>Tasty Talk</p>
+            </div>
+          </div>
+
+          <div className="md:max-w-lg">
+            <p className="md:ml-25">
+              Get Exclusive Early Access and Stay Informed About Product
+              Updates, Events, and More!
+            </p>
+            <div className="flex justify-between items-center border-b border-[#D9D9D9] py-5 md:mt-5 md:ml-25">
+              <input
+                type="email"
+                placeholder="Enter your email"
+                className="w-full placeholder:font-sans placeholder:text-[#999999]"
+              />
+              <img src="/images/arrow.svg" alt="arrow" />
+            </div>
+          </div>
+        </div>
+
+        <div className="copyright-box">
+          <p>Copyright © 2026 Spylt - All Rights Reserved</p>
+          <div className="flex items-center gap-7">
+            <p>Privacy Policy</p>
+            <p>Terms of Sеrvice</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FooterSection;
